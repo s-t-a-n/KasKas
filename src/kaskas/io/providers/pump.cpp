2d11754518811f335e7807d9b72492017fc03084
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2024-09-21 20:52:17 +0200

    pump: use prefix increment for ISR

--- src/kaskas/io/providers/pump.cpp
+++ src/kaskas/io/providers/pump.cpp
@@ -1,13 +1,12 @@
 #include "kaskas/subsystems/fluidsystem.hpp"
 
 namespace kaskas::io {
-
 namespace {
 // when the case arrises, make this a scalable solution
 volatile uint32_t s_counter = 0;
 } // namespace
 
-void interrupt_callback() { s_counter++; }
+void interrupt_callback() { ++s_counter; }
 
 uint32_t Pump::interrupt_counter() { return s_counter; }
 void Pump::reset_interrupt_counter() { s_counter = 0; }
